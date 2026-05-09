a2cc4ec26bf4c222766c36c7a3d19b23707e33f3
Ayush Raj <shaakuni@outlook.in>
2026-05-09 20:21:21 +0530

    Add files via upload

---
+++ BlackJack_game/Blackjack.py
@@ -0,0 +1,82 @@
+import random
+
+def deal_card():
+    """Returns a random card from the deck."""
+    cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+    card = random.choice(cards)
+    return card
+
+def calculate_score(cards):
+    """Take a list of cards and return the score calculated from them."""
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+def compare(u_score, c_score):
+    if u_score == c_score:
+        return "Draw😒"
+    elif c_score == 0:
+        return "Lose, opponent has Blackjack😱"
+    elif u_score == 0:
+        return "Win with a Blackjack😎"
+    elif u_score > 21:
+        return "You went over. You lose😕"
+    elif c_score > 21:
+        return "Opponent went over. You win😁"
+    elif u_score > c_score:
+        return "You win!😉"
+    else:
+        return "You loose😤"
+
+def play_game():
+    print("""
+88          88                       88        88                       88         
+88          88                       88        ""                       88         
+88          88                       88                                 88         
+88,dPPYba,  88 ,adPPYYba,  ,adPPYba, 88   ,d8  88 ,adPPYYba,  ,adPPYba, 88   ,d8   
+88P'    "8a 88 ""     `Y8 a8"     "" 88 ,a8"   88 ""     `Y8 a8"     "" 88 ,a8"    
+88       d8 88 ,adPPPPP88 8b         8888[     88 ,adPPPPP88 8b         8888[      
+88b,   ,a8" 88 88,    ,88 "8a,   ,aa 88`"Yba,  88 88,    ,88 "8a,   ,aa 88`"Yba,   
+8Y"Ybbd8"'  88 `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a 88 `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a  
+                                              ,88                                  
+                                            888P"                                  
+    """)
+    user_card = []
+    computer_card = []
+    computer_score = -1
+    user_score = -1
+
+    is_game_over = False
+
+    for _ in range(2):
+        user_card.append(deal_card())
+        computer_card.append(deal_card())
+
+    while not is_game_over:
+        user_score = calculate_score(user_card)
+        computer_score = calculate_score(computer_card)
+        print(f"Your cards: {user_card}, Current score: {user_score}")
+        print(f"Computer's first cards: {computer_card[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+            if user_should_deal == 'y':
+                user_card.append(deal_card())
+            else:
+                is_game_over = True
+
+    while computer_score != 0 and computer_score < 17:
+        computer_card.append(deal_card())
+        computer_score = calculate_score(computer_card)
+
+    print(compare(user_score, computer_score))
+
+while input("Do you want to play a game of Blackjack? (y/n): ").lower() == "y":
+    play_game()
