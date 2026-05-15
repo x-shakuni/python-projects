aa8af1ac27939f709f1581156adc9f1682573115
Ayush Raj <shaakuni@outlook.in>
2026-05-15 23:10:22 +0530

    Add files via upload

--- QuizGame/data.py
+++ QuizGame/data.py
@@ -1,15 +1,50 @@
+# question_data = [
+#     {"text": "A slug's blood is green.", "answer": "True"},
+#     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+#     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+#     {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+#     {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to "
+#              "eat.", "answer": "True"},
+#     {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+#     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+#     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+#     {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+#     {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+#     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+#     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+# ]
+
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to "
-             "eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+    {"response_code": 0,
+     "results": [
+         {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+          "question": "The logo for Snapchat is a Bell.", "correct_answer": "False",
+          "incorrect_answers": ["True"]},
+         {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+          "question": "The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;",
+          "correct_answer": "True", "incorrect_answers": ["False"]},
+         {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+          "question": "The Windows 7 operating system has six main editions.",
+          "correct_answer": "True", "incorrect_answers": ["False"]},
+         {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+          "question": "JavaScript derives from a later version of Java",
+          "correct_answer": "False", "incorrect_answers": ["True"]},
+         {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+          "question": "A Mac is not a PC", "correct_answer": "False",
+          "incorrect_answers": ["True"]},
+         {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+          "question": "The name of the programming language AWK is abbreviated from the names of the developers &ndash; Aho, Weinberger, and Kernighan.",
+          "correct_answer": "True", "incorrect_answers": ["False"]},
+         {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+          "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+          "correct_answer": "False", "incorrect_answers": ["True"]},
+         {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+          "question": "The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.",
+          "correct_answer": "False", "incorrect_answers": ["True"]},
+         {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+          "question": "The first IBM PC was released in 1981.", "correct_answer": "True",
+          "incorrect_answers": ["False"]},
+         {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+          "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+          "correct_answer": "False", "incorrect_answers": ["True"]}]}
 ]
