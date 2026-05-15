99fa1a7f9624c2fe3680752fb1d3005697d4e1ba
Ayush Raj <shaakuni@outlook.in>
2026-05-15 23:11:08 +0530

    Add files via upload

--- QuizGame/main.py
+++ QuizGame/main.py
@@ -3,9 +3,9 @@ from data import question_data
 from quiz_brain import QuizBrain
 
 question_bank = []
-for question in question_data:
-    question_text = question['text']
-    question_answer = question['answer']
+for question in question_data[0]["results"]:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
